f2f29b58b574de6e4af8aabb0afc0dee57678f7f
Rafael <vecchirafael@gmail.com>
2022-12-04 15:39:38 -0300

    modelo_docente modificado

--- aplicacao/models/modelo_docente.py
+++ aplicacao/models/modelo_docente.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import json
-from uuid import UUID
+from typing import List
+from uuid import UUID, uuid4
 from django.db import models
 
 from dominio.entidades import Docente
@@ -13,7 +14,7 @@ class ModeloDocente(models.Model):
     email = models.EmailField()
     telefones = models.CharField(max_length=200)
     tipo_de_contratacao = models.CharField(max_length=200)
-    unidade_senai_id = models.ForeignKey() #TODO: continuar daqui!
+    #unidade_senai_id = models.ForeignKey() #TODO: continuar daqui!
     ativo = models.BooleanField()
 
     @classmethod
@@ -24,7 +25,7 @@ class ModeloDocente(models.Model):
             email=entidade.email.valor,
             telefone=json.dumps([telefone.valor for telefone in entidade.telefones]),
             tipo_de_contratacao=entidade.tipo_de_contratacao.valor.value,
-            unidade_senai_id=entidade.unidade_senai_id.valor,
+            #unidade_senai_id=entidade.unidade_senai_id.valor,
             ativo=entidade.ativo
         )
 
@@ -32,6 +33,10 @@ class ModeloDocente(models.Model):
         return Docente.construir(
             nome=str(self.nome),
             id_=UUID(str(self.id)),
-            email=str()
+            email=str(),
+            telefones=json.loads(str(self.telefones)),
+            tipo_de_contratacao=str(self.tipo_de_contratacao),
+            unidade_senai_id=uuid4(),
+            #unidade_senai_id=UUID(str(self.unidade_senai_id)),
             ativo=bool(self.ativo)
         )
